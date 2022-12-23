e10b375197cd230de948322f83ab8104c4d12845
Emily <easchoettler@gmail.com>
2022-12-23 13:56:30 -0500

    Add input field & function for obtaining location value

---
+++ src/data.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+const filterData = (data) => {
+    const weatherObj = {
+        name: data.name,
+        weather: data.weather[0].main,
+        description: data.weather[0].description,
+        temp: data.main.temp,
+        feels_like: data.main.feels_like,
+        humidity: data.main.humidity,
+        temp_max: data.main.temp_max,
+        temp_min: data.main.temp_min,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset
+    }
+    console.log(weatherObj);
+    return weatherObj;
+}
+
+async function getData(location) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${  location  }&APPID=a19a9a696056416ad8f7e5a7816ff8f9`, {mode: 'cors'})
+        const weatherData = await response.json();
+        console.log(weatherData);
+        filterData(weatherData);
+    } catch(err) {
+        console.log(err);
+    }
+  }
+
+const getLocation = (input) => {
+    const location = input.value;
+    getData(location);
+}
+
+export default getLocation;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-/* eslint-disable no-console */
-function filterData(data) {
-    const weatherObj = {
-        name: data.name,
-        weather: data.weather[0].main,
-        description: data.weather[0].description,
-        temp: data.main.temp,
-        feels_like: data.main.feels_like,
-        humidity: data.main.humidity,
-        temp_max: data.main.temp_max,
-        temp_min: data.main.temp_min,
-        sunrise: data.sys.sunrise,
-        sunset: data.sys.sunset
-    }
-    console.log(weatherObj);
-}
+import getLocation from "./data";
 
-async function getData(location) {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${  location  }&APPID=a19a9a696056416ad8f7e5a7816ff8f9`, {mode: 'cors'})
-        const weatherData = await response.json();
-        console.log(weatherData);
-        filterData(weatherData);
-    } catch(err) {
-        console.log(err);
-    }
-  }
+const input = document.querySelector("#location");
+const button = document.querySelector("#search");
 
-getData("London");
+button.addEventListener("click", () => {
+    getLocation(input);
+})
+
+
