7be9d33f7983b30a1edcd1c7e5ecada0ae38a794
Emily <easchoettler@gmail.com>
2022-12-27 15:20:45 -0500

    Create grid formatting for weather info

--- src/showData.js
+++ src/showData.js
@@ -12,17 +12,23 @@ const capitalizeFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 
 const showData = (obj) => {
     const content = document.querySelector(".content");
+    const locationArea = document.querySelector(".location");
+    const tempArea = document.querySelector(".temp");
+    const iconArea = document.querySelector(".icon");
+    const detailsArea = document.querySelector(".details");
 
-    const location = createElement("h1", obj.name, content);
-    const fButton = createElement("button", "°F", content);
-    const cButton = createElement("button", "°C", content);
-    const temp = createElement("p", `${convertToF(obj.temp)}°F`, content);
-    const weather = createElement("p", obj.weather, content);
-    const description = createElement("p", capitalizeFirst(obj.description), content);
-    const feels_like = createElement("p", `Feels like: ${convertToF(obj.feels_like)}°F`, content);
-    const humidity = createElement("p", `Humidity: ${obj.humidity}%`, content);
-    const temp_max = createElement("p", `High: ${convertToF(obj.temp_max)}°F`, content);
-    const temp_min = createElement("p", `Low: ${convertToF(obj.temp_min)}°F`, content);
+    const location = createElement("h1", obj.name, locationArea);
+    const fButton = createElement("button", "°F", locationArea);
+    const cButton = createElement("button", "°C", locationArea);
+    const temp = createElement("p", `${convertToF(obj.temp)}°F`, tempArea);
+    //const weather = createElement("p", obj.weather, iconArea); //use this for pic matching?
+    const description = createElement("p", capitalizeFirst(obj.description), iconArea);
+    const feels_like = createElement("p", `Feels like: ${convertToF(obj.feels_like)}°F`, detailsArea);
+    const humidity = createElement("p", `Humidity: ${obj.humidity}%`, detailsArea);
+    const temp_max = createElement("p", `High: ${convertToF(obj.temp_max)}°F`, detailsArea);
+    const temp_min = createElement("p", `Low: ${convertToF(obj.temp_min)}°F`, detailsArea);
+
+    location.classList.add("location-header");
 
     // Toggle between F and C
     fButton.addEventListener("click", () => {
